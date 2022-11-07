9c40f928ec9696d88756c433765151cfb410b6be
Shakhawat2 <shohugmahbub@gmail.com>
2022-11-07 21:40:07 +0600

    server is running on browser

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.send("Assignment 11 server")
+})
+
+app.listen(port, () =>{
+    console.log("Server is Running on ", port);
+})
